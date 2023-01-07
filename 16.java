efc45802fa19dc04767568b4f505f1d6c6cb3a22
Thanmaya Garimella <112673461+thanmaya1@users.noreply.github.com>
2023-01-07 10:21:07 +0530

    Create 16.java

---
+++ 16.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+      int count = 0;
+    int result = 0;
+    for (char c : s.toCharArray()) {
+        if (c == 'L') {
+            count++;
+        } else {
+            count--;
+        }
+        if (count == 0) {
+            result++;
+        }
+    }
+    return result; 
+    }
+}
